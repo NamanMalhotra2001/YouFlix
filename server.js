0b128fc24d44a835f95dc05237439351c7998e13
NamanMalhotra2001 <74060787+NamanMalhotra2001@users.noreply.github.com>
2022-03-17 18:40:11 +0530

    mongoose connected

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
-
 const app = express();
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+mongoose
+	.connect(process.env.MONGO_URL)
+	.then(() => console.log('Connection successful!'));
 
 app.listen(5000, () => {
 	console.log('Server running...');
