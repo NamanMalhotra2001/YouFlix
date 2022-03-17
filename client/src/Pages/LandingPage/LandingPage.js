632daa916e7042493ebe58f407f64449f3d001aa
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-03-17 18:36:10 +0530

    background image issue to be resolved

--- client/src/Pages/LandingPage/LandingPage.js
+++ client/src/Pages/LandingPage/LandingPage.js
@@ -3,15 +3,19 @@ import './LandingPage.css';
 function LandingPage() {
 	return (
 		<div className='Container'>
-			<div className='lPage'>
+			<div className='header'>
 				<h4 className='appName'>YouFlix</h4>
 				<button className='signIn'>Sign-In</button>
-				<h1 className='mainHeading'>
-					Enjoy unlimted streaming anywhere, anytime
-				</h1>
-				<h4 className='subHeading'>
-					Easy cancellation. No strings attached
-				</h4>
+			</div>
+			<div className='mainBody'>
+				<div className='headings'>
+					<h1 className='mainHeading'>
+						Enjoy unlimted streaming anywhere, anytime
+					</h1>
+					<h4 className='subHeading'>
+						Easy cancellation. No strings attached.
+					</h4>
+				</div>
 			</div>
 		</div>
 	);
