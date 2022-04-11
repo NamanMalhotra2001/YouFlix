100ba8748e41db2635e1a251a15d8a7abd1b9470
NamanMalhotra2001 <74060787+NamanMalhotra2001@users.noreply.github.com>
2022-04-11 17:19:05 +0530

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,94 +1,97 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import { MdOutlineArrowForwardIos } from 'react-icons/md';
 
 function LandingPage() {
-  return (
-    <Wrapper>
-      <div className="header">
-        <h1 className="appName">YouFlix</h1>
-        <button className="signIn">Sign-In</button>
-      </div>
-      <div className="mainBodyContainer">
-        <div className="mainBody">
-          <div className="headings">
-            <h1 className="mainHeading">
-              Enjoy unlimted streaming anywhere, anytime
-            </h1>
-            <h4 className="subHeading">
-              Easy cancellation. No strings attached.
-            </h4>
-            <div className="getStartHolder">
-              <button className="getStart">Get Started {'>'} </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<div className='header'>
+				<h1 className='appName'>YouFlix</h1>
+				<button className='signIn'>Sign-In</button>
+			</div>
+			<div className='mainBodyContainer'>
+				<div className='mainBody'>
+					<div className='headings'>
+						<h1 className='mainHeading'>
+							Enjoy unlimted streaming anywhere, anytime
+						</h1>
+						<h4 className='subHeading'>
+							Easy cancellation. No strings attached.
+						</h4>
+						<div className='getStartHolder'>
+							<button className='getStart'>
+								Get Started <MdOutlineArrowForwardIos />
+							</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</Wrapper>
+	);
 }
 
 export default LandingPage;
 
 const Wrapper = styled.div`
-  .mainBodyContainer {
-	padding-top: 150px;
-    min-height: 300vh;
-    background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 1)),
-      url("/images/bg.jpg");
-    background-size: contain;
-  }
+	.mainBodyContainer {
+		padding-top: 150px;
+		min-height: 300vh;
+		background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 1)),
+			url('/images/bg.jpg');
+		background-size: contain;
+	}
 
-  .subHeading {
-    text-align: center;
-  }
+	.subHeading {
+		text-align: center;
+	}
 
-  .header {
-    background-color: white;
-    height: 90px;
-  }
+	.header {
+		background-color: white;
+		height: 90px;
+	}
 
-  .mainHeading {
-	font-size: 70px;
-    text-align: center;
-  }
+	.mainHeading {
+		font-size: 70px;
+		text-align: center;
+	}
 
-  .headings {
-    color: aliceblue;
-  }
+	.headings {
+		color: aliceblue;
+	}
 
-  .signIn {
-	font-size: large;
-	align-items: center;
-	justify-content: center;
-    height: 35px;
-    width: 100px;
-    color: aliceblue;
-    background-color: red;
-    border-radius: 3px;
-    display: flex;
-    position: absolute;
-    top: 20px;
-    right: 10px;
-    border: none;
-    outline: none;
-  }
+	.signIn {
+		font-size: large;
+		align-items: center;
+		justify-content: center;
+		height: 35px;
+		width: 100px;
+		color: aliceblue;
+		background-color: red;
+		border-radius: 3px;
+		display: flex;
+		position: absolute;
+		top: 20px;
+		right: 10px;
+		border: none;
+		outline: none;
+	}
 
-  .getStartHolder {
-    display: flex;
-    justify-content: center;
-    width: 100%;
-  }
+	.getStartHolder {
+		display: flex;
+		justify-content: center;
+		width: 100%;
+	}
 
-  .getStart {
-	font-size: xx-large;
-	font: bold;
-	align-items: center;
-	justify-content: center;
-	height: 75px;
-    width: 250px;
-    border: none;
-    outline: none;
-    color: aliceblue;
-    margin-top: 40px;
-    background-color: red;
-  }
+	.getStart {
+		display: flex;
+		font-size: xx-large;
+		align-items: center;
+		justify-content: center;
+		height: 75px;
+		width: 250px;
+		border: none;
+		outline: none;
+		color: aliceblue;
+		margin-top: 40px;
+		background-color: red;
+	}
 `;
