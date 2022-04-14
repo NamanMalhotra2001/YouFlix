7be4c1da6e3e2fe7cce6ce01068de6bb31acff20
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-04-14 16:53:49 +0530

    tab details need to be changed

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -163,10 +163,8 @@ function LandingPage() {
               </AccordionSummary>
               <AccordionDetails>
                 <Typography className="tabDetails">
-                  Netflix is flexible. There are no annoying contracts and no
-                  commitments. You can easily cancel your account online in two
-                  clicks. There are no cancellation fees – start or stop your
-                  account anytime.
+                  You are subscribed until you die. Then after you die we will
+                  sell your data and your soul to Satan. Good day.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -189,10 +187,7 @@ function LandingPage() {
               </AccordionSummary>
               <AccordionDetails>
                 <Typography className="tabDetails">
-                  Youflix has an extensive library of feature films,
-                  documentaries, TV shows, anime, award-winning Youflix
-                  originals, and more. Watch as much as you want, anytime you
-                  want.
+                  All the latest episodes of failure management.
                 </Typography>
               </AccordionDetails>
             </Accordion>
@@ -215,12 +210,8 @@ function LandingPage() {
               </AccordionSummary>
               <AccordionDetails>
                 <Typography className="tabDetails">
-                  The Youflix Kids experience is included in your membership to
-                  give parents control while kids enjoy family-friendly TV shows
-                  and films in their own space. Kids profiles come with
-                  PIN-protected parental controls that let you restrict the
-                  maturity rating of content kids can watch and block specific
-                  titles you don’t want kids to see.
+                  NO. Lots of adult content. But they'll watch anyways so don't
+                  bother.
                 </Typography>
               </AccordionDetails>
             </Accordion>
