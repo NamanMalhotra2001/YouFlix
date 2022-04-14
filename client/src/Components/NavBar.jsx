1e301c6c09e5ea839eae395967b9f24ab2fb6a17
NamanMalhotra2001 <74060787+NamanMalhotra2001@users.noreply.github.com>
2022-04-14 15:29:32 +0530

    yo yo

--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,43 +1,43 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 function NavBar() {
-  return (
-    <Wrapper>
-      <h1 className="appName">YouFlix</h1>
-      <button className="signIn">Sign-In</button>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<h1 className='appName'>YouFlix</h1>
+			<button className='signIn'>Sign-In</button>
+		</Wrapper>
+	);
 }
 export default NavBar;
 
 const Wrapper = styled.div`
-  display: flex;
-  background-color: white;
-  height: 90px;
-  justify-content: space-between;
+	display: flex;
+	background-color: white;
+	height: 90px;
+	justify-content: space-between;
 	align-items: center;
 	padding-inline: 10px;
 
-  .appName {
+	.appName {
 		user-select: none;
-    font-size: 45px;
-    color: #ff0000e6;
-  }
+		font-size: 45px;
+		color: #ff0000e6;
+	}
 
-  .signIn {
-    cursor: pointer;
-    font-size: large;
-    align-items: center;
-    justify-content: center;
-    height: 35px;
-    width: 100px;
-    color: aliceblue;
-    background-color: red;
-    border-radius: 3px;
-    display: flex;
-    top: 20px;
-    right: 10px;
-    border: none;
-    outline: none;
-  }
+	.signIn {
+		cursor: pointer;
+		font-size: large;
+		align-items: center;
+		justify-content: center;
+		height: 35px;
+		width: 100px;
+		color: aliceblue;
+		background-color: red;
+		border-radius: 3px;
+		display: flex;
+		top: 20px;
+		right: 10px;
+		border: none;
+		outline: none;
+	}
 `;
--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -1,12 +1,16 @@
+import NavBar from 'Components/NavBar';
 import styled from 'styled-components';
 
 function SignupPage() {
-	return <Wrapper>SignUp</Wrapper>;
+	return (
+		<>
+			<NavBar />
+			<Wrapper>SignUp</Wrapper>
+		</>
+	);
 }
 export default SignupPage;
 const Wrapper = styled.div`
 	min-height: 300vh;
-	background: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 1)),
-		url('/images/bg.jpg');
-	background-size: contain;
+	background: white;
 `;
