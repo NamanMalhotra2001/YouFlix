2e99ce0f4d57629467bd19de9d7b13012ada2496
Akshit Chaturvedi <akshitc2228@gmail.com>
2022-06-23 16:21:47 +0100

    Sign-up page updated
    Some touch up may be required

--- client/src/pages/SignupPage.jsx
+++ client/src/pages/SignupPage.jsx
@@ -5,7 +5,15 @@ function SignupPage() {
 	return (
 		<>
 			<NavBar />
-			<Wrapper>SignUp</Wrapper>
+			<Wrapper>
+				<div className='signupContent'>
+					<h1 className='mainHeading'>Create a free account to start your membership</h1>
+					<p className='additionalText'>You may add additional text here if required</p>
+					<input className='credentialsEmail' placeholder='  Email'></input>
+					<input className='credentialsPassword' placeholder='  Add a password'></input>
+					<button className='submit'>NEXT</button>
+				</div>
+			</Wrapper>
 		</>
 	);
 }
@@ -13,4 +21,59 @@ export default SignupPage;
 const Wrapper = styled.div`
 	min-height: 300vh;
 	background: white;
+
+	.signupContent {
+		display: flex;
+		flex-direction: column;
+		text-align: center;
+		align-items: center;
+		justify-content: center;
+	}
+
+	.mainHeading {
+		padding-top: 50px;
+		text-align: left;
+		font-size: 42px;
+		width: 470px;
+	}
+
+	.additionalText {
+		padding-top: 10px;
+		align-items: left;
+		text-align: left;
+		justify-content: left;
+	}
+
+	.credentialsEmail {
+		border: solid;
+		border-width: 1px;
+		border-color: black;
+		margin-top: 50px;
+		width: 470px;
+		height: 65px;
+	}
+
+	.credentialsPassword {
+		border: solid;
+		border-width: 1px;
+		border-color: black;
+		margin-top: 10px;
+		width: 470px;
+		height: 65px;
+	}
+
+	.submit {
+		display: flex;
+		font-size: xx-large;
+		align-items: center;
+		justify-content: center;
+		cursor: pointer;
+		border: none;
+		outline: none;
+		margin-top: 40px;
+		height: 75px;
+		width: 470px;
+		color: aliceblue;
+		background-color: red;
+	}
 `;
